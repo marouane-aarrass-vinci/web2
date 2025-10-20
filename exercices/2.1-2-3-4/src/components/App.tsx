e56eb44bccdc8d23576b6566006278038bca81f2
marouane-aarrass-vinci <marouane.student.vinci.be>
2025-10-20 18:09:28 +0200

    new:ex2.2

--- exercices/2.1-2-3-4/src/components/App.tsx
+++ exercices/2.1-2-3-4/src/components/App.tsx
@@ -1,68 +1,73 @@
-import "./App.css"
-const PageTitle = ({ title }: { title: string }) => <h1>{title}</h1>;
+import "./App.css";
 
-interface CinemaProps {
-  name:string;
-  movie1Title:string;
-  movie1Director:string;
-  movie2Title:string;
-  movie2Director:string;
-}
+// --- Définition des types ---
+type Movie = {
+  title: string;
+  director: string;
+};
 
-const Cinema = ({
-  name,
-  movie1Title,
-  movie1Director,
-  movie2Title,
-  movie2Director,
-}: CinemaProps) => (
-  <div>
-    <h2>{name}</h2>
-    <ul>
-      <li>
-        <strong>{movie1Title}</strong> - Réalisateur : {movie1Director}
-      </li>
-      <li>
-        <strong>{movie2Title}</strong> - Réalisateur : {movie2Director}
-      </li>
-    </ul>
-  </div>
-);
+type PageTitleProps = {
+  title: string;
+};
 
+type CinemaProps = {
+  name: string;
+  movie1: Movie;
+  movie2: Movie;
+};
+
+// --- Composants ---
+const PageTitle = ({ title }: PageTitleProps) => {
+  return <h1>{title}</h1>;
+};
+
+const Cinema = ({ name, movie1, movie2 }: CinemaProps) => {
+  return (
+    <div>
+      <h2>{name}</h2>
+      <ul>
+        <li>
+          <strong>{movie1.title}</strong> - Réalisateur : {movie1.director}
+        </li>
+        <li>
+          <strong>{movie2.title}</strong> - Réalisateur : {movie2.director}
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+// --- Composant principal ---
 const App = () => {
-  const pageTitle = "Informations sur les films dans les cinémas"
+  const pageTitle = "Informations sur les films dans les cinémas";
 
   const cinema1Name = "UGC DeBrouckère";
-  const cinema1Movie1Title = "Film 1 - DeBrouckère";
-  const cinema1Movie1Director = "Director A";
-  const cinema1Movie2Title = "Film 2 - DeBrouckère";
-  const cinema1Movie2Director = "Director B";
+  const movie1: Movie = {
+    title: "HAIKYU-THE DUMPSTER BATTLE",
+    director: "Susumu Mitsunaka",
+  };
+  const movie2: Movie = {
+    title: "GOODBYE JULIA",
+    director: "Mohamed Kordofani",
+  };
 
   const cinema2Name = "UGC Toison d'Or";
-  const cinema2Movie1Title = "Film 1 - Toison d'Or";
-  const cinema2Movie1Director = "Director C";
-  const cinema2Movie2Title = "Film 2 - Toison d'Or";
-  const cinema2Movie2Director = "Director D";
+  const movie3: Movie = {
+    title: "THE WATCHERS",
+    director: "Ishana Night Shyamalan",
+  };
+  const movie4: Movie = {
+    title: "BAD BOYS: RIDE OR DIE",
+    director: "Adil El Arbi, Bilall Fallah",
+  };
 
   return (
-   <div>
+    <div>
       <PageTitle title={pageTitle} />
 
-      <Cinema
-        name={cinema1Name}
-        movie1Title={cinema1Movie1Title}
-        movie1Director={cinema1Movie1Director}
-        movie2Title={cinema1Movie2Title}
-        movie2Director={cinema1Movie2Director}
-      />
+      <Cinema name={cinema1Name} movie1={movie1} movie2={movie2} />
 
-      <Cinema
-        name={cinema2Name}
-        movie1Title={cinema2Movie1Title}
-        movie1Director={cinema2Movie1Director}
-        movie2Title={cinema2Movie2Title}
-        movie2Director={cinema2Movie2Director}
-      />
+      <Cinema name={cinema2Name} movie1={movie3} movie2={movie4} />
     </div>
   );
 };
